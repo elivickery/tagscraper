4f3979c27a99d9e92506f24c9a277047259fdd8a
Eli <eli@Elis-MacBook-Air.local>
2017-09-17 20:44:53 -0700

    Increase openuri timeout limit to fix server timeout issue on heroku

--- app/controllers/routes.rb
+++ app/controllers/routes.rb
@@ -5,7 +5,7 @@ get '/' do
 end
 
 get '/scrape' do
-    @source = open(params[:url]).read
+    @source = open(params[:url], :read_timeout => 30).read
     @source = @source.gsub('<','<br/>&#60;').gsub('>','&#62;<br/>')
     doc_string = Nokogiri::HTML(open(params[:url])).children
     tag_arr = doc_string.search('*').map(&:name)
