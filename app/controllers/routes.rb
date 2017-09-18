40e4fa3d240ccb92d5206c53e52d3f8ad1eb3b99
Eli <eli@Elis-MacBook-Air.local>
2017-09-17 17:45:56 -0700

    Create tag count hash and iterate through tags on index page

--- app/controllers/routes.rb
+++ app/controllers/routes.rb
@@ -5,8 +5,10 @@ end
 get '/scrape' do
     @source = open(params[:url]).read
     @source = @source.gsub('<','<br/>&#60;').gsub('>','&#62;<br/>')
-
     doc_string = Nokogiri::HTML(open(params[:url])).children
+    tag_arr = doc_string.search('*').map(&:name)
+    @tag_counts = Hash.new(0)
+    tag_arr.each { |tag| @tag_counts[tag] += 1}
 
     erb :index
 end
