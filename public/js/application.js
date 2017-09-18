52d0fcb66711fafaa931be787d13d3bc55d98f2e
Eli <eli@Elis-MacBook-Air.local>
2017-09-17 19:44:24 -0700

    Add ajax jquery and corresponding styles

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
-    $('#tag-list li').on('click', function(e){
+    prettyPrint();
+
+    $('#results').on('click', "#tag-list li", function(e){
+        console.log('listening');
         $tag = $('#tag-list').find(this).find('span').text();
         $regex = new RegExp("<[/]?" + $tag + ".*","g");
         $spans = $('pre').find('span');
@@ -8,6 +11,21 @@ $(document).ready(function() {
         $spans.filter(function() {
             return $(this).text().match($regex);
         }).addClass('highlighted');
+    });
+
+    $('#scraper form').on('submit',function(e){
+        e.preventDefault();
+        $('#results').html('<div class="waiting"><p>Generating your code...</p></div>');
+        $data = $(this).serialize();
+
+        $.ajax({
+            url: "/scrape",
+            data: $data,
+            success: function(response) {
+                $('#results').html(response);
+                prettyPrint();
+            }
+        });
     });
 
 });
